c581bf142e76d2caac92ad3e1a64d11027ca69ef
sumarkwar <sumarakor09@gmail.com>
2026-04-17 20:42:22 +0530

    fix blank page + port

--- server.js
+++ server.js
@@ -48,5 +48,11 @@ app.post("/login", async (req, res) => {
 
     res.send("Login success");
 });
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
